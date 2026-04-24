0b7ccde61a761c19efc9a32239b3a5ad5dbada48
Zarinh0 <lazaropmunoz@gmail.com>
2026-04-24 16:11:55 +0200

    Commit inicial

---
+++ test/FuncionesEvalTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuncionesEvalTest {
+
+    @Test
+    void esPerfecto() {
+    }
+
+    @Test
+    void getTipoClima() {
+    }
+
+    @Test
+    void contarApariciones() {
+    }
+
+    @Test
+    void invertirArray() {
+    }
+
+    @Test
+    void encontrarMayor() {
+    }
+}
